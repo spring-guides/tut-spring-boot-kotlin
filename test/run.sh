94ffc574d0b54e37c07d8a0ab7e879c5f4d3120f
Sebastien Deleuze <sdeleuze@pivotal.io>
2018-05-11 14:26:11 +0200

    Add Travis config and script

---
+++ test/run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd $(dirname $0)
+
+cd ..
+
+./mvnw clean package
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf target
+
+./gradlew build
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
