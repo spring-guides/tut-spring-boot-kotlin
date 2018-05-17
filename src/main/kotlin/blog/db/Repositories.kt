3e686fbccd74562812d2e0ac63a68e5a9e68da2f
visakha <viz.ply@gmail.com>
2018-05-17 15:37:11 -0400

    Test for HttpApi passes

---
+++ src/main/kotlin/blog/db/Repositories.kt
@@ -0,0 +1,9 @@
+package blog.db
+
+import org.springframework.data.repository.CrudRepository
+
+interface ArticleRepository : CrudRepository<Article, Long> {
+	fun findAllByOrderByAddedAtDesc(): Iterable<Article>
+}
+
+interface UserRepository : CrudRepository<User, String>
