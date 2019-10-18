49b96c92d924ba6a8bfabb3edf4c7810f997f405
Sebastien Deleuze <sdeleuze@pivotal.io>
2019-10-18 12:17:59 +0200

    Leverage @ConfigurationProperties scanning

--- src/main/kotlin/com/example/blog/BlogApplication.kt
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -1,11 +1,9 @@
 package com.example.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@EnableConfigurationProperties(BlogProperties::class)
 class BlogApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/com/example/blog/HttpControllersTests.kt
+++ src/test/kotlin/com/example/blog/HttpControllersTests.kt
@@ -4,6 +4,7 @@ import com.ninjasquad.springmockk.MockkBean
 import io.mockk.every
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -11,6 +12,7 @@ import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest
+@EnableConfigurationProperties(BlogProperties::class)
 class HttpControllersTests(@Autowired val mockMvc: MockMvc) {
 
 	@MockkBean
