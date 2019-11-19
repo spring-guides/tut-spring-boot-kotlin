06a552462fd02d3cf01bce7478f264abc37063c6
Sébastien Deleuze <sdeleuze@pivotal.io>
2019-11-19 12:44:15 +0100

    Upgrade to Kotlin 1.3.60

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	kotlin("plugin.jpa") version "1.3.50"
 	id("org.springframework.boot") version "2.2.1.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
-	kotlin("jvm") version "1.3.50"
-	kotlin("plugin.spring") version "1.3.50"
-	kotlin("plugin.allopen") version "1.3.50"
-	kotlin("kapt") version "1.3.50"
+	kotlin("jvm") version "1.3.60"
+	kotlin("plugin.spring") version "1.3.60"
+	kotlin("plugin.allopen") version "1.3.60"
+	kotlin("plugin.jpa") version "1.3.60"
+	kotlin("kapt") version "1.3.60"
 }
 
 group = "com.example"
