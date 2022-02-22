6f242e3ef9960ab1f5d3bd630b348347bcfc54da
cdthompso1 <chris@agilegraphics.com>
2022-02-22 15:41:23 -0500

    Update IntegrationTests.kt
    import org.junit.jupiter.api.TestInstance

--- src/test/kotlin/com/example/blog/IntegrationTests.kt
+++ src/test/kotlin/com/example/blog/IntegrationTests.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
