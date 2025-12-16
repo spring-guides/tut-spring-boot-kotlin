359db5d523382a0fedc17ad5edbc55bf42c99cbe
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-12-16 12:48:41 +0100

    Fix Jackson groupId
    For Jackson 3, should be tools.jackson.module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.springframework.boot:spring-boot-starter-mustache")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("tools.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
