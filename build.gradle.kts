5e8b3fb37bf6e00477f3e32f96b168dbf4009851
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-12-19 14:04:41 +0100

    Upgrade to Spring Boot 4.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "4.0.0"
+    id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.2.21"
     kotlin("plugin.spring") version "2.2.21"
